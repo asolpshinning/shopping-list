e7293170bd536d096023502a9479f0fc21260993
asolpshinning <asolpshinning14@gmail.com>
2023-10-01 23:43:17 -0500

    Change to use query

--- backend/clients/db/pg/shopping.go
+++ backend/clients/db/pg/shopping.go
@@ -31,9 +31,8 @@ func (c *client) GetShoppingList(ctx context.Context) ([]*openapi.ShoppingItem, 
 }
 
 func (c *client) AddShoppingItem(ctx context.Context, item openapi.ShoppingItem) error {
-	sess := *c.client
-	defer sess.Close()
-	_, err := sess.Collection("items").Insert(item)
+	query := "INSERT INTO " + config.DB_SHOPPING_TABLE + " (name, description, quantity, purchased) VALUES ($1, $2, $3, $4)"
+	err := ExecPgDBCommand(query, item.Name, item.Description, item.Quantity, item.Purchased)
 	if err != nil {
 		log(ctx).Errorf("failed to add item: %v", err)
 		return errors.New("failed to add item")
@@ -42,8 +41,9 @@ func (c *client) AddShoppingItem(ctx context.Context, item openapi.ShoppingItem)
 }
 
 func (c *client) EditShoppingItem(ctx context.Context, item openapi.ShoppingItem) error {
-	sess := *c.client
-	err := sess.Collection("items").Find("id = ?", item.Id).Update(item)
+	query := "UPDATE " + config.DB_SHOPPING_TABLE + " SET name = $1, description = $2, quantity = $3, purchased = $4 WHERE id = $5"
+	log(ctx).Infof("boolean purchased is: %v", item.Purchased)
+	err := ExecPgDBCommand(query, item.Name, item.Description, item.Quantity, item.Purchased, item.Id)
 	if err != nil {
 		log(ctx).Errorf("failed to edit item: %v", err)
 		return errors.New("failed to edit item")
@@ -52,8 +52,8 @@ func (c *client) EditShoppingItem(ctx context.Context, item openapi.ShoppingItem
 }
 
 func (c *client) DeleteShoppingItem(ctx context.Context, itemID int) error {
-	sess := *c.client
-	err := sess.Collection("items").Find("id = ?", itemID).Delete()
+	query := "DELETE FROM " + config.DB_SHOPPING_TABLE + " WHERE id = $1"
+	err := ExecPgDBCommand(query, itemID)
 	if err != nil {
 		log(ctx).Errorf("failed to delete item: %v", err)
 		return errors.New("failed to delete item")
